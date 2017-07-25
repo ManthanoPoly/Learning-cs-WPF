6455bf51253923749334219cb0e288c8753a6a21
ManthanoPoly <mm1985ad@hotmail.co.uk>
2017-07-25 23:11:25 +0200

    finishing first WPF form with some very basic C# code

--- Learning-cs-WPF/Learning-cs-WPF/MainWindow.xaml.cs
+++ Learning-cs-WPF/Learning-cs-WPF/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Learning_cs_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// https://www.youtube.com/watch?v=Vjldip84CXQ&list=PLrW43fNmjaQVYF4zgsD0oL9Iv6u23PI6M
 
     public partial class MainWindow : Window
     {
@@ -28,7 +29,42 @@ namespace Learning_cs_WPF
 
         private void btnApply_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show($"The description is {this.txtDescription.Text}");
+        }
 
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            this.chkWeld.IsChecked = this.chkAssembly.IsChecked = this.chkPlasma.IsChecked =
+                this.chkLaser.IsChecked = this.chkPurchase.IsChecked = this.chkLathe.IsChecked =
+                this.chkDrill.IsChecked = this.chkFold.IsChecked = this.chkRoll.IsChecked =
+                this.chkSaw.IsChecked = false;
+
+        }
+
+        private void chk_Checked(object sender, RoutedEventArgs e)
+        {
+            /// casting workes with this parentheses. sender, which is an object is cast into the thing left of itself in ()
+            /// .Content accesses now the object sender cast as Checkbox, this Content is automatically cast into string
+            /// but manually casting into string is done by (string) left of ((CheckBox)...
+            this.txtLength.Text += $"{(string)((CheckBox)sender).Content} ";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /// the messy looking way
+            /// this.txtNote.Text = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+            if (this.txtNote == null)
+                return;
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+
+            this.txtNote.Text = (string)value.Content;
+
+        }
+
+        private void txtSupplierName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.txtMass.Text = this.txtSupplierName.Text;
         }
     }
 }
--- learning-diary.md
+++ learning-diary.md
@@ -6,3 +6,11 @@
 
 2017.07.25 06:00 continuing WPF/xaml
 
+2017.07.25 21:50 continuing WPF/xaml, finished that video... want to do more on C# and remember something I found which included SQLite
+
+* want to come back to this though because of view models [WPF UI Programming (C#)](https://www.youtube.com/playlist?list=PLrW43fNmjaQVYF4zgsD0oL9Iv6u23PI6M)
+* [# SQLite Tutorial - 1. Setting up a local SQLITE Database to work with your C# Projects](https://www.youtube.com/watch?v=TrJcKHMe6Y8)
+
+2017.07.25 22:36 
+
+* ok, I'm done now. [continue here](https://youtu.be/sAAKu_SsaoA?list=PLiNd7TUPozDET_shXjcaYQiXscVDGIZkU&t=43) but first I have to manually create this WPF form which he skipped... This is going to be a good practise.
